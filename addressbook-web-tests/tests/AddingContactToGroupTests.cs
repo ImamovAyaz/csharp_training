36bbf6a0772f5a8a87cd0e49d1b606e2b9d4eec9
Имамов Аяз Алмазович <Ayaz.Imamov@norbit.ru>
2021-04-06 01:47:43 +0500

    Задание №17. Исправление задания с исключениями

--- addressbook-web-tests/tests/AddingContactToGroupTests.cs
+++ addressbook-web-tests/tests/AddingContactToGroupTests.cs
@@ -12,13 +12,38 @@ namespace AddressBookWebTests
         [Test]
         public void TestAddingContactToGroup()
         {
-            GroupData group = GroupData.GetAll()[0]; //берём группу с нулевым индексом
-            List<ContactDate> oldList = group.GetContacts(); //получаем список всех контактов, содержащихся в группе
+            GroupData group = new GroupData();
+            List<GroupData> groups = GroupData.GetAll(); //берём группу с нулевым индексом
+            if (groups.Count == 0)
+            {
+                GroupData addgroup = new GroupData(GenerateRandomString(10));
+                app.Groups.Create(addgroup);
+                group = GroupData.GetAll()[0];
+            }
+            else
+            {
+                group = GroupData.GetAll()[0];
+            }
+            List<ContactDate> AllContacts = ContactDate.GetAll();          
+            if (AllContacts.Count == 0)
+            {
+                ContactDate Addcontact = new ContactDate(GenerateRandomString(50), GenerateRandomString(100));
+                app.Contacts.AddNewContact(Addcontact);
+                AllContacts = group.GetContacts();
+            }
+            List<ContactDate> oldList = group.GetContacts();
+            if (ContactDate.GetAll().Except(oldList).FirstOrDefault() == null)
+            {
+
+                ContactDate Addcontact = new ContactDate(GenerateRandomString(50), GenerateRandomString(100));
+                app.Contacts.AddNewContact(Addcontact);
+                //group = GroupData.GetAll()[0];
+            }
+            oldList = group.GetContacts(); //получаем список всех контактов, содержащихся в группе
             ContactDate contact = ContactDate.GetAll().Except(oldList).First(); //получаем первый попавшийся контакт, который содержится в группе с нулевым индексом
 
             // действия 
             app.Contacts.AddContactToGroup(contact, group); //метод добавления контакта в группу, которые принимает соответственно контакт, который необходимо добавить и группу, куда надо это сделать
-
 
             List<ContactDate> newList = group.GetContacts(); //записываем получившуюся группу
             oldList.Add(contact); //добавляем к старому списку тот контакт, который добавили в конкретную группу
--- addressbook-web-tests/tests/RemoveContactToGroupTest.cs
+++ addressbook-web-tests/tests/RemoveContactToGroupTest.cs
@@ -13,25 +13,37 @@ namespace AddressBookWebTests
         [Test]
         public void TestRemoveContactToGroup()
         {
-            //ContactDate contact2 = ContactDate.GetAll()[0];
-            //List<GroupData> oldList = contact2.GetGroups();
-            //GroupData group = GroupData.GetAll().Except(oldList).First();
-
-            GroupData group = GroupData.GetAll()[0]; //берём группу с нулевым индексом
+            GroupData group = new GroupData();
+            List<GroupData> groups = GroupData.GetAll(); //берём группу с нулевым индексом
+            if (groups.Count == 0)
+            {
+                GroupData addgroup = new GroupData(GenerateRandomString(10));
+                app.Groups.Create(addgroup);
+                group = GroupData.GetAll()[0]; //берём группу с нулевым индексом
+            }
+            else
+            {
+                group = GroupData.GetAll()[0]; //берём группу с нулевым индексом
+            }
+            List<ContactDate> AllContacts = ContactDate.GetAll();
+            if (AllContacts.Count == 0)
+            {
+                ContactDate Addcontact = new ContactDate(GenerateRandomString(50), GenerateRandomString(100));
+                app.Contacts.AddNewContact(Addcontact);
+                AllContacts = group.GetContacts();
+            }
             List<ContactDate> oldList = group.GetContacts(); //получаем список всех контактов, содержащихся в группе
-            //if (oldList.Count == 0)
-            //{
-            //    foreach (GroupData groups in GroupData.GetAll())
-            //    {
-            //        if (groups.GetContacts().Count != 0)
-            //        {
-            //            group = groups;
-            //            break;
-            //        }
-            //    }
-            //    ContactDate firstcontact = ContactDate.GetAll()[0];
-            //    oldList.Add(firstcontact);
-            //}
+            //ContactDate Contacts = ContactDate.GetAll().Except(oldList).FirstOrDefault();
+            if ((ContactDate.GetAll().Except(oldList).FirstOrDefault() == null) || (oldList.Count == 0))
+            {
+
+                ContactDate Addcontact = new ContactDate(GenerateRandomString(50), GenerateRandomString(100));
+                app.Contacts.AddNewContact(Addcontact);
+                Addcontact = ContactDate.GetAll().Except(oldList).First();
+                app.Contacts.AddContactToGroup(Addcontact, group);
+                //group = GroupData.GetAll()[0];
+            }
+            oldList = group.GetContacts();
             //ContactDate contact = ContactDate.GetAll().Except(oldList).First(); //получаем первый попавшийся контакт, который содержится в группе с нулевым индексом
             ContactDate contact = oldList[0];
             // действие
